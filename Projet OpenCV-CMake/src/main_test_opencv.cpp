e77dcdb57ec41a028bbcca48cdf012b1692a6b4a
CarlistleZ <CarlistleZHENG@gmail.com>
2019-12-11 09:15:07 +0100

    Get all files in main to launch extract

--- Projet OpenCV-CMake/src/main_test_opencv.cpp
+++ Projet OpenCV-CMake/src/main_test_opencv.cpp
@@ -11,6 +11,8 @@
 #include <thresholding.h>
 #include <SquareExtractor.h>
 #include <matcher.h>
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
 
 using namespace std;
 
@@ -18,20 +20,25 @@ using namespace std;
 #include "opencv2/highgui.hpp"
 using namespace cv;
 
+#include <iostream>
 #include "histogram.hpp"
 
 
-int main (void) {
-//	computeHistogram("histogramme", im);
-//	calc_threshold("threshold", im);
-//    performSift();
+int main () {
+    string basePath = "/home-info/commun/p/p12/5info/irfBD/NicIcon/all-scans/";
+    for (const auto & entry : fs::directory_iterator(basePath)) {
+        std::cout << entry.path() << std::endl;
+        int crossCoordinates[4];
+        // TODO
+        // fill in the coordinate array
+        extract(entry.path(), crossCoordinates[0], crossCoordinates[1], crossCoordinates[2], crossCoordinates[3]);
+    }
     Matcher matcher;
     for (int i = 0; i < 35; ++i) {
         matcher.classifyImage("../ImageResult/Square" + to_string(i) + ".png");
     }
 
 //    std::cout << "Not a match: " << surfTest("../Images/query.png", "../Images/trainWrong.png") << endl;
-
 //    extract("../Images/00000.png", 2145, 548, 294, 3162);
 //    extract("/home-info/commun/p/p12/5info/irfBD/NicIcon/all-scans/02202.png", 2201, 468, 257, 3232);
 //    extract("/home-info/commun/p/p12/5info/irfBD/NicIcon/all-scans/02601.png", 2205, 470, 263, 3232);
